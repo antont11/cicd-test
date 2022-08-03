43bcecced7af10de990c897981e6c23a8d73b395
ANTONT11 <theodoros.antonaros@pfizer.com>
2022-08-03 19:41:24 +0300

    init

---
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/hello")
+public class DemoController {
+
+
+  @GetMapping
+  public ResponseEntity<String> hello() {
+    return new ResponseEntity<>("Hello", HttpStatus.OK);
+  }
+
+}
